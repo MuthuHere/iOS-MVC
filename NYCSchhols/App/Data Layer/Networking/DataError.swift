276476555e69f49fdf2d9ccae3dbc4450c86c49b
Muthu <muthukumar.subramaniam@cplcloud.com>
2024-04-12 18:28:27 +0800

    first commit

---
+++ NYCSchhols/App/Data Layer/Networking/DataError.swift
@@ -0,0 +1,11 @@
+//
+//
+// Copyright © 2024 THALES. All rights reserved.
+//
+    
+
+import Foundation
+
+enum DataError : Error {
+    case networkingError(String)
+}
---
+++ NYCSchhols/App/UI Layer/ViewController/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//
+// Copyright © 2024 THALES. All rights reserved.
+//
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let api : SchoolApiLogic = SchoolDirectoryApi()
+        api.getSchools { result in
+            
+            switch result {
+            case .failure(let error):
+                print(error)
+                break
+            case .success(let schools):
+            
+                if let schools = schools {
+                    print(schools.count)
+                }
+                break
+                
+            }
+        }
+    }
+
+
+}
+
